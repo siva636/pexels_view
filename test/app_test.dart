358f2ec4646038cb70d2c0e4d8d8f5f03ada542c
siva <siva636@gmail.com>
2022-08-22 16:27:06 +0530

    Weiget test fixed

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,11 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pexels_view/src/app.dart';
 import 'package:pexels_view/src/utility/constant/constant.dart';
 
 void main() {
-  testWidgets('App should have the Search widget title',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
-    expect(find.text(title), findsOneWidget);
-  });
+  testWidgets(
+    'App should have the Search widget title',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.widgetWithText(ElevatedButton, 'Search'), findsOneWidget);
+    },
+  );
 }
